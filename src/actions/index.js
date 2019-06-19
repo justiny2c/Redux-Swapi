e4599ed5be4db6f0ad4ac2fa4a166f2a22337afa
justiny2c <justiny2c@gmail.com>
2019-06-19 12:55:34 -0700

    Initial Commit. Index.js set up

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 // we'll need axios
+import axios from "axios";
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
@@ -6,3 +7,17 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+export const FETCH_CHARS_START = "FETCH_CHARS_START";
+
+export const getChars = () => {
+    dispatch({ type: FETCH_CHARS_START });
+    axios
+        .get(`https://swapi.co/api/people/`)
+        .then(res => {
+            dispatch({ type: FETCH_CHARS_SUCCESS, payload: res.data })
+        })
+        .catch(err => {
+            dispatch ({ type: FETCH_CHARS_FAILED, payload: err })
+        })
+}
